2b8f39698e14a833674d5bf69a4e43e601c7be51
Maxim Palchikov <palchmx@gmail.com>
2020-10-19 14:23:46 +0300

    Change theme colors

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,20 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#009688",
+        secondary: "#4caf50",
+        accent: "#cddc39",
+        error: "#f44336",
+        warning: "#ffc107",
+        info: "#00bcd4",
+        success: "#795548",
+      },
+    },
+  },
 });
